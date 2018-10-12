5595a6768c88dfca8dbb8f31d1749c0734f04108
Andrew Seward <awswrd@gmail.com>
2018-10-12 01:45:38 -0700

    refactor: Use MiscTools' version targets

--- build.fsx
+++ build.fsx
@@ -2,64 +2,11 @@
 #r "./packages/ASeward.MiscTools/lib/net471/ASeward.MiscTools.dll"
 
 open ASeward.MiscTools.Versioning
-open ASeward.MiscTools.ActivePatterns
 open Fake
-open System
-open System.IO
+
+FakeTargetStubs.createVersionTargets Target getBuildParam ["src/ASeward.MiscTools/AssemblyInfo.fs"]
 
 let projects = !! "/**/*.fsproj"
-let assemblyInfos = ["src/ASeward.MiscTools/AssemblyInfo.fs"]
-
-module AsmInf = ASeward.MiscTools.Versioning.AssemblyInfo
-module AsmInfSemVer =
-  let private _writeAllText filePath contents = File.WriteAllText (filePath, contents)
-  let private _write filePath semVer =
-    filePath
-    |> File.ReadAllText
-    |> AsmInf.replaceAssemblyVersion semVer
-    |> AsmInf.replaceAssemblyFileVersion semVer
-    |> AsmInf.replaceAssemblyInformationalVersion semVer
-    |> _writeAllText filePath
-  let iterMap fn =
-    assemblyInfos
-    |> List.iter (fun filePath ->
-        filePath
-        |> AsmInf.tryReadInfoVersion
-        |> Option.map fn
-        |> Option.iter (_write filePath)
-    )
-
-let promptFor name =
-  (sprintf "Did not provide value for parameter '%s'. Please specify (default: ''): " name)
-  |> Console.Write
-  |> Console.ReadLine
-  |> fun str -> str.Trim ()
-
-Target "version:major" (fun _ -> AsmInfSemVer.iterMap SemVer.incrMajor)
-Target "version:minor" (fun _ -> AsmInfSemVer.iterMap SemVer.incrMinor)
-Target "version:patch" (fun _ -> AsmInfSemVer.iterMap SemVer.incrPatch)
-Target "version:pre" (fun _ ->
-  let map =
-    "pre"
-    |> getBuildParam
-    |> function
-        | NullOrWhiteSpace -> promptFor "pre"
-        | str -> str
-    |> fun pre -> SemVer.setPre pre
-
-  AsmInfSemVer.iterMap map
-)
-Target "version:meta" (fun _ ->
-  let map =
-    "meta"
-    |> getBuildParam
-    |> function
-        | NullOrWhiteSpace -> promptFor "meta"
-        | str -> str
-    |> fun meta -> SemVer.setMeta meta
-
-  AsmInfSemVer.iterMap map
-)
 
 Target "Build:Release" (fun _ ->
   projects
