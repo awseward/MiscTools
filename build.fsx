d408ba34a06bfbd8d2ffee85f448e3b170c3518d
Andrew Seward <awswrd@gmail.com>
2018-10-14 00:46:35 -0700

    refactor: Use FakeTargetStubs.printReleaseNotes

--- build.fsx
+++ build.fsx
@@ -2,7 +2,6 @@
 #r "./packages/ASeward.MiscTools/lib/net471/ASeward.MiscTools.dll"
 
 open ASeward.MiscTools
-open ASeward.MiscTools.ActivePatterns
 open Fake
 open System
 
@@ -11,6 +10,15 @@ open System.Net
 ServicePointManager.SecurityProtocol <- ServicePointManager.SecurityProtocol ||| SecurityProtocolType.Tls ||| SecurityProtocolType.Tls11 ||| SecurityProtocolType.Tls12
 
 Versioning.FakeTargetStubs.createVersionTargets Target getBuildParam ["src/ASeward.MiscTools/AssemblyInfo.fs"]
+
+Target
+  ReleaseNotes.FakeTargetStubs.targetName
+  (fun _ ->
+    ReleaseNotes.FakeTargetStubs.printReleaseNotes
+      (getBuildParamOrDefault)
+      "awseward"
+      "misctools"
+  )
 
 let projects = !! "/**/*.fsproj"
 
@@ -37,32 +45,6 @@ Target "Paket:Push" (fun _ ->
         ApiKey = environVar "BUGSNAG_NET_NUGET_API_KEY"
         WorkingDir = paketOutputDir
     }
-)
-
-Target "release:notes" (fun _ ->
-  let org = "awseward"
-  let repo = "misctools"
-  let token = Environment.GetEnvironmentVariable "GITHUB_TOKEN"
-
-  printfn ""
-
-  match getBuildParam "base" with
-  | NullOrWhiteSpace -> ()
-  | baseCommit ->
-      let headCommit = getBuildParamOrDefault "head" "master"
-      printfn "https://github.com/%s/%s/compare/%s...%s" org repo baseCommit headCommit
-      printfn ""
-
-  "prs"
-  |> getBuildParam
-  |> fun str -> str.Split ';'
-  |> Array.map int
-  |> Array.toList
-  |> ReleaseNotes.doTheThingAsync token org repo
-  |> Async.RunSynchronously
-  |> printfn "%s"
-
-  printfn ""
 )
 
 "Paket:Pack" <== ["Build:Release"]
