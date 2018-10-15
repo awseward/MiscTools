6de1bd9076763ec3010360e38da8b04007e6a80e
Andrew Seward <awswrd@gmail.com>
2018-10-14 17:40:29 -0700

    misc: Some minor cleanup

--- temp/shims.fsx
+++
@@ -1,9 +0,0 @@
-#r "../packages/FAKE/tools/FakeLib.dll"
-
-namespace ASeward.MiscTools
-
-module Shims =
-  open Fake
-  let Target = Target
-  let RunTargetOrDefault = RunTargetOrDefault
-  let (<==) = (<==)
