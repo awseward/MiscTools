ab9177254f48a053cb2616f71ec817226e14ad3e
Andrew Seward <awswrd@gmail.com>
2018-10-27 13:25:26 -0700

    chore: Remove now-unnecessary scripts

--- fake.macOS.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-./paket.macOS.sh restore --fail-on-checks
-
-dotnet restore
-
-mono ./packages/fakebuild/FAKE/tools/FAKE.exe build.fsx $@ --removeLegacyFakeWarning
--- fake.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-./paket.sh restore --fail-on-checks
-
-./packages/fakebuild/FAKE/tools/FAKE.exe build.fsx $@ --removeLegacyFakeWarning
